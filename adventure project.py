6d9fc80e9bec7efdd89e351f18eee52a16a8c1e0
The-Indian-Coder-20 <untitledlofi.clean@gmail.com>
2024-07-26 22:32:30 +0900

    third commit

--- adventure project.py
+++ adventure project.py
@@ -7,19 +7,19 @@ time.sleep(3)
 points = 0
 lives = 3
 countries = {"Japan": ["Tokyo"], "India": ["New Dehli", "new dehli", "dehli", "Dehli"], "France": ["Paris"], "Italy": ["Rome"], "United States of America": ["Washington D.C.", "washington d.c.", "washington D.C.", "D.C."]}
+x = f"Your final score was {points}."
 
 for country,capitals in countries.items():
-    x = str(input(f"What is the capital of {country}?: "))
-    if x in capitals:
+    y = str(input(f"What is the capital of {country}?: "))
+    if y in capitals:
         print("You are correct!")
         points += 1
     else:
         print("You are wrong!")
         lives -= 1
-    if lives > 0 and countries:
-        print("Game over, you won!")
+    if lives == 0:
+        print("You have lost all of your lives! You lost! " + x)
         break
-    elif lives == 0:
-        print("You have lost all of your lives! You lost!")
-        break
-print(f"Your final score was {points}")
+
+if lives > 0:
+    print("You won! " + x)
