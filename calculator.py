42a202577d29604643736dd24c51cc106b908f22
The-Indian-Coder-20 <untitledlofi.clean@gmail.com>
2024-07-27 10:21:54 +0900

    first commit

--- calculator.py
+++ calculator.py
@@ -7,17 +7,29 @@ operator = input("""What operator would you like to use?
         / (division) 
         
         """)
+value = ""
 #Gives a alphabetical value instead of only symbols
-if operator == "+":
-    value = "addition"
-elif operator == "-":
-    value = "subtraction"
-elif operator == "*":
-    value = "multiplication"
-else:
-    value = "division"
+while True:
+    if operator == "+":
+        value = "addition"
+        print(f"The chosen operator for this process is {value} ({operator})")
+        break
+    elif operator == "-":
+        value = "subtraction"
+        print(f"The chosen operator for this process is {value} ({operator})")
+        break
+    elif operator == "*":
+        value = "multiplication"
+        print(f"The chosen operator for this process is {value} ({operator})")
+        break
+    elif operator == "/":
+        value = "division"
+        print(f"The chosen operator for this process is {value} ({operator})")
+        break
+    else:
+        print("You have chosen an invalid operator.")
+        exit()
 #Prints alphabetical value as well as the symbol operator.
-print(f"The chosen operator for this process is {value} ({operator})")
 #Asks user for numbers to use.
 num1 = float(input("What is the first number you would like to use?: "))
 num2 = float(input("What is the second number you would like to use?: "))
@@ -37,7 +49,7 @@ elif operator == "*":
     x = float(num1 * num2)
     y = round(x, 3)
     print(y)
-else:
+elif operator == "/":
     x = float(num1 / num2)
     y = round(x, 3)
     print(y)
