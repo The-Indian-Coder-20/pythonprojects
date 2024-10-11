362d11f744d064f7237acc02df2ee925175ed0a6
The-Indian-Coder-20 <untitledlofi.clean@gmail.com>
2024-10-11 14:22:07 +0900

    "Removed commented out code block at top of file, added a for loop that prints numbers 1-10."

---
+++ Random number game #2.py
@@ -0,0 +1,42 @@
+import random
+
+num1, num2, num3, num4 = random.randint(1,5), random.randint(1,5), random.randint(1,5), random.randint(1,5)
+while num1 == num2 or num1 == num3 or num1 == num4 or num2 == num3 or num3 == num4 or num3 == num4:
+    num1, num2, num3, num4 = random.randint(1,5), random.randint(1,5), random.randint(1,5), random.randint(1,5)
+numList = [num1, num2, num3, num4]
+emptyNumList = ["a", "b", "c", "d"]
+print("Please guess the numbers and place of the 4 digit number")
+print(emptyNumList)
+print(numList)
+num = int(input("What number would you like to guess?: "))
+place = input("What position would you like to put the number in?: ")
+while "a" in emptyNumList or "b" in emptyNumList or "c" in emptyNumList or "d" in emptyNumList:
+    if num in numList:
+        if str(place) in emptyNumList:
+            if numList.index(num) == emptyNumList.index(place):
+                print("You got a position and number correct!")
+                emptyNumList[numList.index(num)] = num
+                print(emptyNumList)
+            else:
+                print("You got the number correct, but not the position!")
+                place = input("What position would you like to put the number in?: ")
+                while emptyNumList.index(place) != numList.index(num):
+                    place = input("What position would you like to put the number in?: ")
+                print("You got a position and number correct!")
+                emptyNumList[numList.index(num)] = num
+                print(emptyNumList)
+    else:
+        print("You didn't guess anything correct :(")
+        num = int(input("What number would you like to guess?: "))
+        place = input("What position would you like to put the number in?: ")
+    if "a" in emptyNumList or "b" in emptyNumList or "c" in emptyNumList or "d" in emptyNumList:
+        num = int(input("What number would you like to guess?: "))
+        place = input("What position would you like to put the number in?: ")
+    else:
+        break
+print("Congratulations! You guessed all of the numbers!")
+playAgain = input("Would you like to play again?: ")
+if playAgain.lower() == "yes" or playAgain.lower() == "y":
+    x = "hrish"
+else:
+    print("See you next time!")
