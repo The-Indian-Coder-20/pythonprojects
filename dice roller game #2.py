1c1cff67073f05b00ca937585d58a33059dec346
The-Indian-Coder-20 <158476323+The-Indian-Coder-20@users.noreply.github.com>
2024-10-14 20:26:55 +0900

    First commit

---
+++ dice roller game #2.py
@@ -0,0 +1,29 @@
+import random
+
+maxCount = 10
+score1, score2 = 0, 0
+i = 1
+die1, die2 = 0, 0
+while i <= maxCount:
+    while die1 == die2:
+        die1, die2 = random.randint(1,6), random.randint(1,6)
+    die1, die2 = random.randint(1,6), random.randint(1,6)
+    score1 += die1
+    score2 += die2
+    if die1 == 6 and die2 == 6:
+        die1, die2 = random.randint(1,6), random.randint(1,6)
+        score1 += die1
+        score2 += die2
+    elif die2 == 6:
+        die2 = random.randint(1,6)
+        score2 += die2
+    elif die1 == 6:
+        die1 = random.randint(1,6)
+        score1 += die1
+    i += 1
+if score1 > score2:
+    print(f"Player one won with a total score of {score1}!")
+elif score1 == score2:
+    print(f"Both players got the same score of {score1}!")
+else:
+    print(f"Player two won with a total score of {score2}!")
