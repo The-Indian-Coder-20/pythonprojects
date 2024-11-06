abf4a1500fe51d21f77f8302ee76dec020a8b12f
Skibidi coder <158476323+The-Indian-Coder-20@users.noreply.github.com>
2024-11-06 12:48:55 +0900

    yes

--- counting sort attempt.py
+++ counting sort attempt.py
@@ -1,8 +1,16 @@
-countingList = [2, 0, 4, 6, 8, 10]
+countingList = [2, 3, 0, 2, 3, 2]
+midList = []
 finalList = []
 i = 0
 while i <= max(countingList):
-    finalList.append(0)
-print(finalList, max(countingList))
-    
-    
+    midList.append(0)
+    i += 1
+for i in countingList:
+    midList[i] += 1
+print(midList)
+for i in range(len(midList)):
+    j = 1
+    while j <= midList[i]:
+        finalList.append(i)
+        j += 1
+print(finalList)
