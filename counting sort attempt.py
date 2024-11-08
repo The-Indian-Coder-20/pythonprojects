e54161a5ef2031a6bfbb8e7ed1b97cde7c189768
Skibidi coder <158476323+The-Indian-Coder-20@users.noreply.github.com>
2024-11-08 10:28:10 +0900

    Update counting sort attempt.py

--- counting sort attempt.py
+++ counting sort attempt.py
@@ -1,16 +1,13 @@
-countingList = [2, 3, 0, 2, 3, 2]
-midList = []
-finalList = []
-i = 0
-while i <= max(countingList):
-    midList.append(0)
-    i += 1
-for i in countingList:
-    midList[i] += 1
-print(midList)
-for i in range(len(midList)):
-    j = 1
-    while j <= midList[i]:
-        finalList.append(i)
-        j += 1
-print(finalList)
+def countingSort(countingList):
+    midList, finalList, i = [], [], 0
+    while i <= max(countingList):
+        midList.append(0)
+        i += 1
+    for i in countingList:
+        midList[i] += 1
+    for i in range(len(midList)):
+        j = 1
+        while j <= midList[i]:
+            finalList.append(i)
+            j += 1
+    return finalList
