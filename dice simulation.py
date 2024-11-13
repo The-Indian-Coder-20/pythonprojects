3451fdc3296b13248b36656c92e7acf1a9db458a
The-Indian-Coder-20 <untitledlofi.clean@gmail.com>
2024-11-13 17:07:59 +0900

    adding stuff

--- dice simulation.py
+++ dice simulation.py
@@ -14,10 +14,16 @@ for j in range(len(finalList)):
         finalList[j].append(currentDiceNum - tempList.count(1))
         currentDiceNum -= tempList.count(1)
         tempList.clear()
-    print(f"Trial {j}: Completed with results {finalList[j]}")
-
+avgList, lenLists = [], []
+for list in finalList:
+    lenLists.append(len(list))
+for i in range(len(finalList)):
+    if len(finalList[i]) != lenLists[0]:
+        print(finalList[i])
+        for num in range(lenLists[0]-len(finalList[i])):
+            finalList[i].append(0)
 # Excel File Writing with pandas
-df = pd.DataFrame(finalList).transpose()  # Make rows columns (transpose allows Excel vertical compatibility)
-df.to_excel("dicesimulation.xlsx", index=False, header=[f'Trial {i+1}' for i in range(numberOfLists)], engine='xlsxwriter')
+#df = pd.DataFrame(finalList).transpose()  # Make rows columns (transpose allows Excel vertical compatibility)
+#df.to_excel("dicesimulation.xlsx", index=False, header=[f'Trial {i+1}' for i in range(numberOfLists)], engine='xlsxwriter')
 
 print(f"Simulation results saved in 'dicesimulation.xlsx'")
