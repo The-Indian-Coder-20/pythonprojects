21ae3197170cbf05207ef8b64289ad61b1070acf
The-Indian-Coder-20 <untitledlofi.clean@gmail.com>
2024-11-13 12:19:19 +0900

    Test

--- radix sort attempt.py
+++ radix sort attempt.py
@@ -3,8 +3,14 @@ def radixSort(numList):
     while x <= len(str(max(numList))):
         numSortList.append([])
         x += 1
-    print(numSortList)
     for i in range(len(numList)):
-        print("yes")
-
+        for j in range(len(str(numList[i]))):
+            numSortList[j].append(int(str(numList[i])[j]))
+    for y in numSortList:
+        y = y.sort()
+    for z in range(len(numSortList[0])):
+        for a in range(len(numSortList)):
+            finalList.append(int(f"{numSortList[a][z]}"))
+    print(numSortList)
+    print(finalList)
 radixSort([124, 515, 891, 238, 901, 231, 234, 512, 123, 723])
